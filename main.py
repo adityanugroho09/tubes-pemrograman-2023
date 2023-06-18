d3d32e1581e28b746f5ff57498211f081f677c83
unknown <fillipusaditya2@gmail.com>
2023-06-18 15:28:42 +0700

    Udah Bisa Pilih Jenis Suit

--- main.py
+++ main.py
@@ -79,13 +79,16 @@ def track_fingers():
                 cv2.putText(frame, "Back", (frame.shape[1] // 2 - 200, frame.shape[0] // 2 - 200),
                             cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
                 cv2.destroyAllWindows()
-                main_game()
+                main_game(selected_menu)
+
             elif selected_menu == "Jawa":
                 # Erase all menu displays
                 cv2.rectangle(frame, (20, frame.shape[0] // 2 - 50), (frame.shape[1] - 20, frame.shape[0] // 2 + 50), (0, 0, 0),-1)
                 # Display the message
                 cv2.putText(frame, "You are in Jawa Game", (frame.shape[1] // 2 - 180, frame.shape[0] // 2),
                             cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+                cv2.destroyAllWindows()
+                main_game(selected_menu)
 
             # Display the frame with the finger positions and menu selection
             cv2.imshow('Finger Tracking', frame)
@@ -98,79 +101,115 @@ def track_fingers():
     cap2.release()
     cv2.destroyAllWindows()
 
-def main_game():
+def main_game(selected_menu):
+    # Penggunaan Camera
     cap = cv2.VideoCapture(0)
     cap.set(3,640)
     cap.set(4,480)
 
     detector = HandDetector(maxHands=1)
 
+    # Timer
     timer = 0
     stateResult = False
     startGame = False
 
+    # Array Score
     scores = [0,0] # [AI, PLAYER]
+
+    selected_menu = selected_menu
 
     while True:
         imgBG = cv2.imread("Resources/BG.png")
         success, img = cap.read()
 
+        # Mengatur Ukuran Layar Kamera
         imgScaled = cv2.resize(img,(0,0),None,0.875,0.875)
         imgScaled = imgScaled[:,80:480]
 
         # Find Hands
         hands, img = detector.findHands(imgScaled)
 
+        # Game Dimulai
         if startGame:
 
             if stateResult is False:
                 timer = time.time() - initialTime
                 cv2.putText(imgBG, str(int(timer)), (605, 435), cv2.FONT_HERSHEY_PLAIN, 6, (255, 0, 255), 4)
 
+                # Timer
                 if timer > 3:
                     stateResult = True
                     timer = 0
+
+                    # Jika tangan terdeteksi maka :
                     if hands:
                         playerMove = None
                         hand = hands[0]
                         fingers = detector.fingersUp(hand)
 
-                        if fingers == [0,0,0,0,0]:
-                            playerMove = 1
-                        if fingers == [1,1,1,1,1]:
-                            playerMove = 2
-                        if fingers == [0,1,1,0,0]:
-                            playerMove = 3
+                        # Deteksi Jari dan Penentuan Palyer Move
+                        if selected_menu == "English":
+                            if fingers == [0,0,0,0,0]:
+                                playerMove = 1
+                            if fingers == [1,1,1,1,1]:
+                                playerMove = 2
+                            if fingers == [0,1,1,0,0]:
+                                playerMove = 3
+                            # Generate Nomor Acak
+                            randomNumber = random.randint(1, 3)
+                        elif selected_menu == "Jawa":
+                            if fingers == [1,0,0,0,0]:
+                                playerMove = 4
+                            if fingers == [0,1,0,0,0]:
+                                playerMove = 5
+                            if fingers == [0,0,0,0,1]:
+                                playerMove = 6
+                            randomNumber = random.randint(4, 6)
 
-                        randomNumber = random.randint(1, 3)
+                        # Akses Gambar
                         imgAI = cv2.imread(f'Resources/{randomNumber}.png',cv2.IMREAD_UNCHANGED)
                         imgBG = cvzone.overlayPNG(imgBG, imgAI, (149, 310))
 
-                        # Palyer Wins
-                        if (playerMove == 1 and randomNumber == 3) \
-                            or (playerMove == 2 and randomNumber == 1) \
-                                or (playerMove == 3 and randomNumber == 2):
-                            scores[1] += 1
+                        if selected_menu == "English":
+                            # Kondisi Player Menang
+                            if (playerMove == 1 and randomNumber == 3) \
+                                or (playerMove == 2 and randomNumber == 1) \
+                                    or (playerMove == 3 and randomNumber == 2):
+                                scores[1] += 1
                         
-                        # AI Wins
-                        if (playerMove == 3 and randomNumber == 1) \
-                            or (playerMove == 1 and randomNumber == 2) \
-                                or (playerMove == 2 and randomNumber == 3):
-                            scores[0] += 1
+                            # Kondisi AI Menang
+                            if (playerMove == 3 and randomNumber == 1) \
+                                or (playerMove == 1 and randomNumber == 2) \
+                                    or (playerMove == 2 and randomNumber == 3):
+                                scores[0] += 1
+                            print(playerMove)
+                        if selected_menu == "Jawa":
+                            # Palyer Wins
+                            if (playerMove == 4 and randomNumber == 5) \
+                                or (playerMove == 5 and randomNumber == 6) \
+                                    or (playerMove == 6 and randomNumber == 4):
+                                scores[1] += 1
+                    
+                            # AI Wins
+                            if (playerMove == 5 and randomNumber == 4) \
+                                or (playerMove == 6 and randomNumber == 5) \
+                                    or (playerMove == 4 and randomNumber == 6):
+                                scores[0] += 1
 
-                        print(playerMove)
-
+        #  Atur Ukuran Display Camera
         imgBG[234:654, 795:1195] = imgScaled
 
-
+        # Tata Letak Gambar AI
         if stateResult:
             imgBG = cvzone.overlayPNG(imgBG, imgAI, (149, 310))
 
+        # Tata Letak Score
         cv2.putText(imgBG, str(scores[0]), (410, 215), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 6)
         cv2.putText(imgBG, str(scores[1]), (1112, 215), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 6)
-        # cv2.imshow("Image", img)
-        cv2.imshow("imgBG", imgBG) # Menampilkan tampilan layar
-        # cv2.imshow("Scaled", imgScaled)
+
+        # Menampilkan tampilan layar
+        cv2.imshow("imgBG", imgBG) 
 
         # Klik S Untuk Memulai Game
         key = cv2.waitKey(1)
